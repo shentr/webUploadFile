4371b62995591cb7f7f3c52ed2425d1b188d869a
lastone <15545016598@163.com>
2016-07-16 23:52:45 +0800

    登录功能

---
+++ webUploadFile/common/postStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webUploadFile.common
+{
+    public class postStatus
+    {
+        public int status;
+        public string message;
+        public postStatus(int sta,string mes)
+        {
+            status = sta;
+            message = mes;
+        }
+    }
+}
